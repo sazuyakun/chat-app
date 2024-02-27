1f1287e15b67a4c87631de8952c0624d8c070da9
Soham Samal <sohamsamal37@gmail.com>
2024-02-27 22:58:45 +0530

    userModel and password hashing

--- server/index.js
+++ server/index.js
@@ -1,16 +1,17 @@
 import express from 'express';
 import * as dotenv from 'dotenv'
+
 import authRoutes from "./routes/auth.routes.js"
 import connectMongo from './database/connectMongo.js';
 
 const app = express();
-
 dotenv.config();
 const PORT = process.env.PORT || 5000;
 
-app.get('/', (req, res) => {
-    res.send('Hello Urvi');
-})
+// app.get('/', (req, res) => {
+//     res.send('Hello Urvi');
+// })
+app.use(express.json()); //parse incoming requests from req.body
 
 app.use("/api/auth", authRoutes)
 
