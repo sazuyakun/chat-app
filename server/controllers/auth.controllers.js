54a919404d2a092f9704e4e234d11570d8052da3
Soham Samal <sohamsamal37@gmail.com>
2024-02-28 20:46:22 +0530

    Finalising signup controllers

--- server/controllers/auth.controllers.js
+++ server/controllers/auth.controllers.js
@@ -5,14 +5,16 @@ export const signup = async (req, res) => {
     try {
         const {fullName, username, password, confirmPassword, gender} = req.body;
 
+        //line 10 -> 29 is for confirming password, hashing password, checking if same username exists and 
+        //giving random profile picture using api
         if(password !== confirmPassword){
             return res.status(400).json({
                 error: "Passwords don't match."
             })
         }
-        const user = await User.findOne({username});
+        const isUsernameExists = await User.findOne({username});
 
-        if(user){
+        if(isUsernameExists){
             return res.status(400).json({
                 error: "Username already exists."
             })
@@ -21,11 +23,13 @@ export const signup = async (req, res) => {
         // HASH PASSWORD HERE
         const salt = await bcrypt.genSalt(10);
         const hashedPassword = await bcrypt.hash(password, salt);
+        
         //https://avatar-placeholder.iran.liara.run/
-
         const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=${username}`
         const girlProfilePic = `https://avatar.iran.liara.run/public/girl?username=${username}`
 
+
+        //33 -> 48 is creating a new user finally and storing it in database
         const newUser = new User({
             fullName,
             username,
@@ -36,7 +40,7 @@ export const signup = async (req, res) => {
 
         await newUser.save();
 
-        res.status(201).json({
+        res.status(201).json({        //if success, sends a json response... check postman
             _id: newUser._id,
             fullName: newUser.fullName,
             username: newUser.username,
