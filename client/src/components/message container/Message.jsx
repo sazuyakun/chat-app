bba90c2d9c239641994a5caeec40413d3e07249b
Soham Samal <sohamsamal37@gmail.com>
2024-03-05 16:48:55 +0530

    App completion - Real time integration left

--- client/src/components/message container/Message.jsx
+++ client/src/components/message container/Message.jsx
@@ -1,20 +1,28 @@
-import React from 'react'
+import { useAuthContext } from "../../context/AuthContext";
+import { extractTime } from "../../utils/extractTime";
+import useConversation from "../../zustand/useConversation";
 
-const Message = () => {
-  return (
-    <div className='chat chat-end'>
-        <div className='chat-image avatar'>
-            <div className='w-10 rounded-full'>
-                <img 
-                    alt='Tailwind CSS chat bubble component'
-                    src = 'https://png.pngtree.com/png-vector/20191110/ourmid/pngtree-avatar-icon-profile-icon-member-login-vector-isolated-png-image_1978396.jpg'
-                />
-            </div>
-        </div>
-        <div className={`chat-bubble text-white bg-blue-500`}>Hi, What's up?</div>
-        <div className=' chat-footer opacity-50 text-xs flex gap-1 items-center'>12:42</div>
-    </div>
-  )
-}
+const Message = ({ message }) => {
+	const { authUser } = useAuthContext();
+	const { selectedConversation } = useConversation();
+	const fromMe = message.senderId === authUser._id;
+	const formattedTime = extractTime(message.createdAt);
+	const chatClassName = fromMe ? "chat-end" : "chat-start";
+	const profilePic = fromMe ? authUser.profilePic : selectedConversation?.profilePic;
+	const bubbleBgColor = fromMe ? "bg-blue-500" : "";
 
-export default Message
+	const shakeClass = message.shouldShake ? "shake" : "";
+
+	return (
+		<div className={`chat ${chatClassName}`}>
+			<div className='chat-image avatar'>
+				<div className='w-10 rounded-full'>
+					<img alt='Tailwind CSS chat bubble component' src={profilePic} />
+				</div>
+			</div>
+			<div className={`chat-bubble text-white ${bubbleBgColor} ${shakeClass} pb-2`}>{message.message}</div>
+			<div className='chat-footer opacity-50 text-xs flex gap-1 items-center'>{formattedTime}</div>
+		</div>
+	);
+};
+export default Message;
